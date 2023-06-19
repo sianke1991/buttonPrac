7ed285dcc8b1e577819eb6bb1cbd13f0899ee76d
sianke1991 <90839850+sianke1991@users.noreply.github.com>
2023-06-19 20:28:45 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-html="buttonHtml" id="buttonHtml">
+  </div>
+  <div>
+    {{buttonHtml}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      buttonHtml: ""
+    }
+  },
+  created() {
+    const link = 'http://google.com';
+    const text = 'google.com';
+    this.buttonHtml = '<button id="btn" onclick="window.open(\''
+                    .concat(link)
+                    .concat('\')">')
+                    .concat(text)
+                    .concat('</button>');
+  }
+}
+</script>
+
+<style>
+#buttonHtml {
+  width: 140px;
+  height: 100px;
+  border: 2px solid grey;
+  background-color: white;
+}
+#btn {
+  background-color: blue;
+  width: 140px;
+  height: 100px;
+}
+</style>
